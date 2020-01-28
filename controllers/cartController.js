3c14d0db09934b10627243eb44a8bfb0091b4cf9
Sean Gayler <seangayler@Seans-MacBook-Pro.local>
2020-01-28 14:13:15 +1000

    Assigned false to cartItems argument of cart view template when cart is empty so that correct page can be rendered.

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -18,7 +18,7 @@ exports.cart_list = function(req, res) {
       /*  Construct a new list of objects to include duplicate menu items in the cart by matching ids 
           in idObjectList with those in the '_id' field of objects within objectList.
       */
-    const objectListDuplicates = new Array();
+    let objectListDuplicates = new Array();
     for (let i = 0; i < idObjectList.length; i++) {
       for (let j = 0; j < objectList.length; j++) {
         if (idObjectList[i].equals(objectList[j]._id)) {
@@ -27,13 +27,15 @@ exports.cart_list = function(req, res) {
         }
       }
     }
-    /*  Calculate the total price of the order by adding prices of all individual items and render the
-        result.
-    */
+    // Calculate the total price of the order by adding prices of all individual items. 
     let totalPrice = 0;
     objectListDuplicates.forEach(menuItem => {
       totalPrice += menuItem.price;
     });
+    // ssign false to objectListDuplicates if there are no menu items in the cart
+    if (objectListDuplicates.length == 0) {
+      objectListDuplicates = false;
+    }
     res.render('cart', { cartItems: objectListDuplicates, totalPrice: totalPrice });
   });
 }
