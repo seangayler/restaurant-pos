7ab1902f4a7f94803062983d7ecfd7402a309624
Sean Gayler <seangayler@Seans-MBP.modem>
2020-01-30 13:03:28 +1000

    Fixed elapsed time bug on orders page.

--- public/javascripts/orderScript.js
+++ public/javascripts/orderScript.js
@@ -9,28 +9,29 @@ setInterval(() => {
 
 // takes time in HH:MM:SS format and increments by 1 second
 function incrementTime(time) {
-  let hours = parseInt(time.slice(0,2), 10);
-  let minutes = parseInt(time.slice(3,5), 10);
-  let seconds = parseInt(time.slice(6,8), 10);
+  let hours = parseInt(time.slice(0,2));
+  let minutes = parseInt(time.slice(3,5));
+  let seconds = parseInt(time.slice(6,8));
   seconds++;
-  switch (60) {
-    case seconds:
-      seconds = 0;
-      minutes++;
-    case minutes:
-      minutes = 0;
-      hours++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes == 60) {
+    minutes = 0;
+    seconds++;
   }
   hours = hours.toString();
   minutes = minutes.toString();
   seconds = seconds.toString();
-  switch (1) {
-    case hours.length:
-      hours = '0' + hours;
-    case minutes.length:
-      minutes = '0' + minutes;
-    case seconds.length:
-      seconds = '0' + seconds;
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
   }
   return `Time elapsed: ${hours}:${minutes}:${seconds}`;
 }
